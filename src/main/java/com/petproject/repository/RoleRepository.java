aae637746f5b49327dd3386474f1cee2f0afa3b6
Vlad <hryshko_vv@ffeks.dnu.edu.ua>
2022-09-04 13:20:41 +0300

    ISSUE SOLVED: Develop Repositories

---
+++ src/main/java/com/petproject/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.petproject.repository;
+
+import com.petproject.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
