accd133d5f190ac6ce0b5d0cdae3bef9477363bb
Vlad <hryshko_vv@ffeks.dnu.edu.ua>
2022-09-05 12:53:50 +0300

    Issue Solved: Realize RoleServiceImpl and create NullEntityReferenceException

--- src/main/java/com/petproject/service/impl/RoleServiceImpl.java
+++ src/main/java/com/petproject/service/impl/RoleServiceImpl.java
@@ -1,33 +1,55 @@
 package com.petproject.service.impl;
 
+import com.petproject.exception.NullEntityReferenceException;
 import com.petproject.model.Role;
+import com.petproject.repository.RoleRepository;
 import com.petproject.service.RoleService;
+import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class RoleServiceImpl implements RoleService {
+
+    private RoleRepository roleRepository;
+
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
     @Override
     public Role create(Role role) {
-        return null;
+        if (role != null) {
+            return roleRepository.save(role);
+        }
+        throw new NullEntityReferenceException("Role cannot be 'null'");
     }
 
     @Override
     public Role readById(long id) {
-        return null;
+        return roleRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Role with id " + id + " not found"));
     }
 
     @Override
     public Role update(Role role) {
-        return null;
+        if (role != null) {
+            readById(role.getId());
+            return roleRepository.save(role);
+        }
+        throw new NullEntityReferenceException("Role cannot be 'null'");
     }
 
     @Override
     public void delete(long id) {
-
+        roleRepository.delete(readById(id));
     }
 
     @Override
     public List<Role> getAll() {
-        return null;
+        List<Role> roles = roleRepository.findAll();
+        return roles.isEmpty() ? new ArrayList<>() : roles;
     }
 }
