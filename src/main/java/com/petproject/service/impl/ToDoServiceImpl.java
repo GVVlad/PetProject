4042e0a1d8354a62e5090cb44761a1fb007229de
Vlad <hryshko_vv@ffeks.dnu.edu.ua>
2022-09-05 16:40:46 +0300

    Developed implementation for ToDoService and UserService

--- src/main/java/com/petproject/service/impl/ToDoServiceImpl.java
+++ src/main/java/com/petproject/service/impl/ToDoServiceImpl.java
@@ -1,38 +1,61 @@
 package com.petproject.service.impl;
 
+import com.petproject.exception.NullEntityReferenceException;
 import com.petproject.model.ToDo;
+import com.petproject.repository.ToDoRepository;
 import com.petproject.service.ToDoService;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class ToDoServiceImpl implements ToDoService {
+    private ToDoRepository toDoRepository;
+
+    public ToDoServiceImpl(ToDoRepository toDoRepository) {
+        this.toDoRepository = toDoRepository;
+    }
+
     @Override
     public ToDo create(ToDo todo) {
-        return null;
+        if (todo == null) {
+            throw new NullEntityReferenceException("ToDo cannot be 'null'");
+        }
+        return toDoRepository.save(todo);
     }
 
     @Override
     public ToDo readById(long id) {
-        return null;
+        return toDoRepository.findById(id).orElseThrow(
+                () -> new NullEntityReferenceException("Cannot find ToDo with given 'id'"));
     }
 
     @Override
     public ToDo update(ToDo todo) {
-        return null;
+
+        if (todo == null) {
+            throw new NullEntityReferenceException("ToDo cannot be 'null'");
+        }
+
+        readById(todo.getId());
+        return toDoRepository.save(todo);
     }
 
     @Override
     public void delete(long id) {
-
+        toDoRepository.delete(readById(id));
     }
 
     @Override
     public List<ToDo> getAll() {
-        return null;
+        List<ToDo> todos = toDoRepository.findAll();
+        return todos.isEmpty() ? new ArrayList<>() : todos;
     }
 
     @Override
     public List<ToDo> getByUserId(long userId) {
-        return null;
+        List<ToDo> toDos = toDoRepository.getByUserId(userId);
+        return toDos.isEmpty() ? new ArrayList<>() : toDos;
     }
 }
--- src/main/java/com/petproject/service/impl/UserServiceImpl.java
+++ src/main/java/com/petproject/service/impl/UserServiceImpl.java
@@ -1,33 +1,53 @@
 package com.petproject.service.impl;
 
+import com.petproject.exception.NullEntityReferenceException;
 import com.petproject.model.User;
+import com.petproject.repository.UserRepository;
 import com.petproject.service.UserService;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class UserServiceImpl implements UserService {
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public User create(User user) {
-        return null;
+        if (user == null) {
+            throw new NullEntityReferenceException("User cannot be 'null'");
+        }
+        return userRepository.save(user);
     }
 
     @Override
     public User readById(long id) {
-        return null;
+        return userRepository.findById(id).orElseThrow(
+                () -> new NullEntityReferenceException("Cannot find user with given 'id'"));
     }
 
     @Override
     public User update(User user) {
-        return null;
+        if (user == null) {
+            throw new NullEntityReferenceException("User cannot be 'null'");
+        }
+        readById(user.getId());
+        return userRepository.save(user);
     }
 
     @Override
     public void delete(long id) {
-
+        userRepository.delete(readById(id));
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        List<User> users = userRepository.findAll();
+        return users.isEmpty() ? new ArrayList<>() : users;
     }
 }
